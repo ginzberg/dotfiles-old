e1d144801c69a440c72086330c8c008ae1ae3e92
ginzberg <rcallen@gmail.com>
2021-10-11 18:00:58 -0700

    remove 3rd display

--- scripts/monitors/monitors.sh
+++ scripts/monitors/monitors.sh
@@ -3,8 +3,7 @@
 # Set sencondary monitor as veritcal orientation, set to the right
 # Set monitor default refresh rates
 xrandr --output DP-2 --primary --mode 1920x1080 --rate 279.75 \
-       --output DP-0 --left-of DP-2 --mode 1920x1080 --rate 143.85 \
-       --output HDMI-0 --right-of DP-2 --mode 1920x1080 --rate 60.0 &
+       --output DP-0 --left-of DP-2 --mode 1920x1080 --rate 143.85 &
 
 # Set BSPWM
 # bspc monitor DP-0 -d 1 2 3 &
