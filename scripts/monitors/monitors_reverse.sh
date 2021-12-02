38fb90e97da0cc8b6c8faca38b215d7ba8817d54
ginzberg <rcallen@gmail.com>
2021-12-02 09:10:57 -0800

    new monitors

--- scripts/monitors/monitors_reverse.sh
+++ scripts/monitors/monitors_reverse.sh
@@ -2,8 +2,8 @@
 
 # Set sencondary monitor as veritcal orientation, set to the right
 # Set monitor default refresh rates
-xrandr --output DP-2 --primary --mode 1920x1080 --rate 360.11 \
-       --output DP-0 --left-of DP-2 --mode 1920x1080 --rate 279.75 &
+xrandr --output DP-2 --mode 1920x1080 --rate 279.75 \
+       --output DP-0 --primary --left-of DP-2 --mode 1920x1080 --rate 360.11 &
 
 # Set BSPWM
 # bspc monitor DP-0 -d 1 2 3 &
