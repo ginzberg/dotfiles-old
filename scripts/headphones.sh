d6399f1c4f5a42699ac71f17b0d42d39fa641b7f
ginzberg <rcallen@gmail.com>
2022-07-11 13:12:18 -0700

    purge unused scripts

--- scripts/headphones.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pactl set-default-sink alsa_output.usb-Samson_Technologies_Samson_G-Track_Pro_53FE38101F183B00-00.analog-stereo
-
-notify-send " setting audio output to headphones"
-
-kill -44 $(pidof dwmblocks)
-
--- scripts/speakers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pactl set-default-sink alsa_output.pci-0000_2d_00.1.hdmi-stereo
-
-notify-send "蓼 setting audio output to speakers"
-
-kill -44 $(pidof dwmblocks)
