c3d647de6ccdc35a66ce9bfec3efcd9ee8ea394d
ginzberg <rcallen@gmail.com>
2022-07-12 15:35:14 -0700

    user-ify nvim

---
+++ nvim/user/mappings.lua
@@ -0,0 +1,9 @@
+local map = vim.api.nvim_set_keymap
+local opt = { noremap = true }
+
+-- map the leader key
+map('n', '<Space>', '', {})
+vim.g.mapleader = ' ' -- 'vim.g' sets global variables
+
+-- nvim-tree
+-- put bindings here
