4cc3f16fe4759c1ba4bbc34d609abb4ccc679108
rallen <rcallen@gmail.com>
2022-12-29 13:30:23 -0800

    script cleanup

--- scripts/litra-auto-on.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-# Send signal to turn on
-# hidapitester --vidpid '046D/C900' --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
-
-# Send signal to turn off
-# hidapitester --vidpid '046D/C900' --open --length 20 --send-output 0x11,0xff,0x04,0x1c
